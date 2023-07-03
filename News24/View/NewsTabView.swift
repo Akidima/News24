1b322a943f8ffda2d6e5ccf591390ffb00e0239a
George Akidima <georgeakidima001@gmail.com>
2023-07-03 19:59:41 +0100

    Inital Commit

--- News24/View/NewsTabView.swift
+++ News24/View/NewsTabView.swift
@@ -6,54 +6,74 @@
 //
 
 import SwiftUI
-
 struct NewsTabView: View {
     @StateObject var articleNewsVM = ArticleNewsVM()
     
-    
     var body: some View {
-        VStack{
-            NavigationView {
-                ArticleListView(articles: articles)
-                    .overlay(overlayView)
-                    .refreshable {
-                        
-                    }
-                    .onAppear {
-                        Task {
-                            await articleNewsVM.loadArticles()
+        NavigationView {
+            ArticleListView(articles: articles)
+                .overlay(overlayView)
+                .task(id: articleNewsVM.fetchTaskToken, loadTask)
+                .refreshable(action: refreshTask)
+               
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        VStack(spacing: 0) {
+                            ImageNavView()
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                CustomSegementedPicker(fetchTaskToken: $articleNewsVM.fetchTaskToken,
+                                                       selectedItemColor: .black,
+                                                       backgroundColor: .clear,
+                                                       selectedItemFontColor: .white,
+                                                       defaultItemColor: .gray)
+                                .task(id: articleNewsVM.fetchTaskToken, loadTask)
+                                
+                                    
+                            }
                         }
                     }
-                    .navigationTitle(articleNewsVM.selectedCategory.text)
-            }
+                }
         }
     }
     
     @ViewBuilder
     private var overlayView: some View {
-        
-    switch articleNewsVM.phase  {
-                
-    case .empty:
-         ProgressView()
-            
-    case .success(let articles) where articles.isEmpty:
-        EmptyPlaceholder(text: "No articles found", imageName: "No-Article")
-
-    case .failure(let error):  RetryView(text: error.localizedDescription) {
+        switch articleNewsVM.phase {
+        case .empty:
+            ProgressView()
+        case .success(let articles) where articles.isEmpty:
+            EmptyPlaceholder(text: "No articles found", imageName: "No-Article")
+        case .failure(let error):
+            RetryView(text: error.localizedDescription, retryAction: refreshTask)
+        default:
+            EmptyView()
+        }
     }
-        default: EmptyView()
-    }
-}
     
-    private var articles:[Article] {
+    private var articles: [Article] {
         if case let .success(articles) = articleNewsVM.phase {
             return articles
         } else {
             return []
         }
     }
+    
+    @Sendable
+       private func loadTask() async {
+           await articleNewsVM.loadArticles()
+       }
+       
+        
+        private func refreshTask() {
+           DispatchQueue.main.async {
+               articleNewsVM.fetchTaskToken = FetchTaskToken(category: articleNewsVM.fetchTaskToken.category, token: Date())
+           }
+       }
 }
+
+
 
 
 struct NewsTabView_Previews: PreviewProvider {
@@ -61,3 +81,15 @@ struct NewsTabView_Previews: PreviewProvider {
         NewsTabView(articleNewsVM: ArticleNewsVM(articles: Article.previewData))
     }
 }
+
+struct ImageNavView: View {
+    var body: some View {
+        Image("title2")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 99, height: 24)
+            
+    }
+}
+
+
