fd02b8e8db37f2bb25337799d265527b60200d40
George Akidima <georgeakidima001@gmail.com>
2023-07-04 21:33:03 +0100

    Inital Commit

--- News24/View/ArticleListView.swift
+++ News24/View/ArticleListView.swift
@@ -12,31 +12,32 @@ struct ArticleListView: View {
     @State private var selectedArticle: Article?
     
     var body: some View {
-        
         NavigationView {
-        List{
-            ForEach(articles) { article in
-                ArticleRowView(article: article)
-                    .onTapGesture {
-                        selectedArticle = article
-                    }
-            }.listRowSeparator(.visible, edges: .bottom)
-        }
-        .listStyle(.plain)
-        .sheet(item: $selectedArticle) {
-            SafariView(url: $0.articleURL)
-                .edgesIgnoringSafeArea(.bottom)
-        }
-            
-        }
+            List{
+                    ForEach(articles) { article in
+                        ArticleRowView(article: article)
+                            .onTapGesture {
+                                selectedArticle = article
+                            }
+                    }.listRowSeparator(.visible, edges: .bottom)
+                }
+                .listStyle(.plain)
+                .sheet(item: $selectedArticle) {
+                    SafariView(url: $0.articleURL)
+                        .edgesIgnoringSafeArea(.bottom)
+                }
+            }
+        
     }
 }
 
 struct ArticleListView_Previews: PreviewProvider{
+    @StateObject static  var articleBookVM = ArticleBookMarkVM.shared
+    
     static var previews: some View {
         NavigationView {
             ArticleListView(articles: Article.previewData)
-                
+                .environmentObject(articleBookVM)
         }
     }
 }
