0bf655a7c08c5d501263b19fa845027cc4f0e6db
George Akidima <georgeakidima001@gmail.com>
2023-07-04 21:31:08 +0100

    Inital Commit

--- News24/ViewModel/ArticleBookMarkVM.swift
+++ News24/ViewModel/ArticleBookMarkVM.swift
@@ -11,5 +11,41 @@ import SwiftUI
 class ArticleBookMarkVM: ObservableObject {
     
     @Published private(set) var bookmarks: [Article] = []
+    private let bookamarkStore = PlistDataStore<[Article]>(filename: "bookmarks")
     
+    static let shared = ArticleBookMarkVM()
+    private init() {
+        Task {
+           await load()
+        }
+    }
+    private func load() async {
+        bookmarks = await bookamarkStore.load() ?? []
+    }
+    
+    func isBookmarked(for article: Article) -> Bool {
+        bookmarks.first { article.id == $0.id } != nil
+    }
+    
+    func addBookmark(for article: Article) {
+        guard !isBookmarked(for: article) else {
+            return
+        }
+        bookmarks.insert(article, at: 0)
+        bookmarkUpdated()
+    }
+    
+    func removeBookmark(for article: Article) {
+        guard let index = bookmarks.firstIndex(where: { $0.id ==  article.id }) else {
+            return
+        }
+        bookmarks.remove(at: index)
+        bookmarkUpdated()
+    }
+    private func bookmarkUpdated() {
+        let bookmarks = self.bookmarks
+        Task {
+            await bookamarkStore.save(bookmarks)
+        }
+    }
 }
